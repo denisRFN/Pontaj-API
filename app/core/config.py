f7dd9d427450f01f996709d98243d8f13a3be976
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-02-25 12:05:59 +0200

    Create config.py

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = "SUPER_SECRET_CHANGE_THIS_IN_PRODUCTION"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+
+settings = Settings()
