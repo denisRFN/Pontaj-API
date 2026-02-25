29bc15055fe335b42a26f6fd86850c89678d6583
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-02-25 10:24:02 +0200

    Update main.py for additional endpoint

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db, init_db
 from app.models.hours import Hours
-
+from app.models.calendar import Calendar
 app = FastAPI()
 
 @app.on_event("startup")
@@ -15,6 +15,20 @@ def create_hours(overtime: str, permission: str, db: Session = Depends(get_db)):
     new_entry = Hours(
         overtime=overtime,
         permission=permission
+    )
+
+    db.add(new_entry)
+    db.commit()
+    db.refresh(new_entry)
+
+    return new_entry
+
+@app.post("/calendar")
+def create_calendar(prezente: str, concediu: str, wfh: str, db: Session = Depends(get_db)):
+    new_entry = Calendar(
+        prezente=prezente,
+        concediu=concediu,
+        wfh=wfh
     )
 
     db.add(new_entry)
