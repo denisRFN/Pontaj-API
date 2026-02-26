478479153e903936b45600ed8a1056a1d31e6b09
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-02-26 12:02:16 +0200

    Update user.py

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
-
+from app.core.security import hash_password
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
