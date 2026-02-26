bae19e85a85234c94b63d2d087beb1b3d0803189
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-02-26 12:26:38 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,9 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "SUPER_SECRET_CHANGE_THIS_IN_PRODUCTION"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-
+    model_config = {"env_file":".env",
+                   "extra":"ignore"}
 
 settings = Settings()
