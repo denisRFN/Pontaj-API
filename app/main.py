c04aaed723976e8b3465810be8137e6bbd9b2119
denisRFN <denisrobertflorentinnegoita@gmail.com>
2026-02-27 13:44:22 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from app.database import init_db
 from app.routers import hours, calendar
 from app.models import user
 from app.routers import auth
-app = FastAPI()
+app = FastAPI(title ="Pontaj API Production",
+              version = "1.0.0")
 
 @app.on_event("startup")
 def on_startup():
@@ -12,5 +13,6 @@ def on_startup():
 app.include_router(hours.router)
 app.include_router(calendar.router)
 app.include_router(auth.router)
+
 
 
